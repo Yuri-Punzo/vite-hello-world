7016426d8af010b52eb48030556c3f9da946983d
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-09 14:57:02 +0100

    intial and final commit + bonus. credo.

---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="my_jumbo">
+        <p>Am I a jumbotron ?</p>
+        <p>...please, just say yes, i always wanted to be a Jumbotron since i was a pixel...</p>
+    </div>
+</template>
+
+<style scoped>
+.my_jumbo {
+    height: 400px;
+    width: 100%;
+    background-color: indigo;
+    color: white;
+}
+
+p {
+    padding: 1rem;
+    font-size: 18px;
+}
+</style>
---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+    padding: 1rem;
+    background-color: #363636;
+    color: white;
+    font-size: 80px;
+}
+</style>
