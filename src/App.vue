3a5e46f4e09560800d630e615a845b4efc1c5f1d
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-10 10:05:19 +0100

    added export name in App

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AppTitle from './components/AppTitle.vue'
 import AppHero from './components/AppHero.vue'
 
 export default {
+    name: "App",
     components: {
         AppTitle,
         AppHero
